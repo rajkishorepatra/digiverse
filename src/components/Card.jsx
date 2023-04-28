320e623b2353e94d4cc16b383702f9a3eb404472
aditya-2403 <47123413+aditya-2403@users.noreply.github.com>
2023-04-28 09:22:29 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
     <>
                 <div className="col-md-4 col-12 mx-auto" >
                   <div className="card">
-                    <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc} />
+                    <img src={props.imgsrc} className="card-img-top" alt="Service_Image" />
                     <div className="card-body">
                       <h5 className="card-title font-weight-bold">{props.title}</h5>
                       <p className="card-text">{props.text}</p>
