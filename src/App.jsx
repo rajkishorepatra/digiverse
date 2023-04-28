bf748c1196c7062db0460828505b323d37b7aa60
aditya-2403 <47123413+aditya-2403@users.noreply.github.com>
2023-04-28 12:14:12 +0530

    update REFUND PATH

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { Switch, Route } from "react-router-dom";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import Courses from "./components/Courses";
 import MyParticles from "./components/MyParticles";
+import RefundPolicy from "./components/Refundpolicy";
 
 
 const App = () => {
@@ -25,6 +26,7 @@ const App = () => {
           <Route exact path="/service" component={Service} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/courses" component={Courses} />
+          <Route exact path="/refundpolicy" component={RefundPolicy} />
           <Redirect to="/" />
         </Switch>
         <Footer />
