72ecdb616594d5ee8d7411c575a57823c9b8694a
aditya-2403 <47123413+aditya-2403@users.noreply.github.com>
2023-05-02 20:16:54 +0530

    fix navbar smooth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,92 +8,90 @@ const Navbar = () => {
   const toggleNavbar = () => setCollapsed(!collapsed);
 
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">
-            <img
-              className="nav-logo mt-2"
-              src={logo}
-              style={{ width: 100 }}
-              alt="Digiverse360 Logo"
-            />
-            <strong> DigiVerse360</strong>
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={toggleNavbar}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}
-            id="navbarNav"
-          >
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink
-                  activeClassName="menu_active"
-                  exact
-                  className="nav-link"
-                  aria-current="page"
-                  to="/"
-                  onClick={toggleNavbar}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  activeClassName="menu_active"
-                  className="nav-link"
-                  to="/about"
-                  onClick={toggleNavbar}
-                >
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  activeClassName="menu_active"
-                  className="nav-link"
-                  to="/service"
-                  onClick={toggleNavbar}
-                >
-                  Services
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  activeClassName="menu_active"
-                  className="nav-link"
-                  to="/courses"
-                  onClick={toggleNavbar}
-                >
-                  Courses
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  activeClassName="menu_active"
-                  className="nav-link"
-                  to="/contact"
-                  onClick={toggleNavbar}
-                >
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          <img
+            className="nav-logo mt-2"
+            src={logo}
+            style={{ width: 100 }}
+            alt="Digiverse360 Logo"
+          />
+          <strong> DigiVerse360</strong>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarNav"
+          aria-expanded={!collapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={toggleNavbar}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                activeClassName="menu_active"
+                exact
+                className="nav-link"
+                aria-current="page"
+                to="/"
+                onClick={toggleNavbar}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                activeClassName="menu_active"
+                className="nav-link"
+                to="/about"
+                onClick={toggleNavbar}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                activeClassName="menu_active"
+                className="nav-link"
+                to="/service"
+                onClick={toggleNavbar}
+              >
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                activeClassName="menu_active"
+                className="nav-link"
+                to="/courses"
+                onClick={toggleNavbar}
+              >
+                Courses
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                activeClassName="menu_active"
+                className="nav-link"
+                to="/contact"
+                onClick={toggleNavbar}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
