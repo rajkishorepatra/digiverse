8fcf3d3282387ae242d9d3952a2d1191bc40860a
aditya-2403 <47123413+aditya-2403@users.noreply.github.com>
2023-06-10 03:20:18 +0530

    Revamp Service page (part 2)

--- src/components/MarketingComparison.jsx
+++ src/components/MarketingComparison.jsx
@@ -1,46 +1,105 @@
-import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const MarketingComparison = () => {
   return (
     <Container>
-      <h2 className='text-white my-4'><strong>Growth Comparison: Digital Marketing vs Traditional Marketing</strong></h2>
+      <h2 className="text-white my-4">
+        <strong>
+          Growth Comparison: Digital Marketing vs Traditional Marketing
+        </strong>
+      </h2>
       <Row>
         <Col md={6}>
-          <Card className='digi-card mark-card'>
+          <Card className="digi-card mark-card">
             <Card.Body>
-              <Card.Title><strong>Digital Marketing</strong></Card.Title>
-              <Card.Text className='text-secondary text-overflow'>
-                Digital marketing has grown exponentially in recent years, as more and more people are spending time online. This growth has been driven by a number of factors, including:
+              <Card.Title>
+                <strong>Digital Marketing</strong>
+              </Card.Title>
+              <Card.Text className="text-secondary text-overflow">
+                Digital marketing has grown exponentially in recent years, as
+                more and more people are spending time online. This growth has
+                been driven by a number of factors, including:
               </Card.Text>
               <ul>
-                <li><span className='text-primary'>The Rise of Social Media:</span> Social media platforms like Facebook, Twitter, and Instagram have given businesses a new way to reach their target audience.</li> <br />
-                <li><span className='text-primary'>The Growth of E-commerce:</span> E-commerce has made it easier for businesses to sell their products and services online, reaching a global audience.</li> <br />
-                <li><span className='text-primary'>The Increasing Use of Mobile Devices:</span> More and more people are using their smartphones and tablets to access the internet, which has created new opportunities for businesses to reach their target.</li>
+                <li>
+                  <span className="text-primary">
+                    The Rise of Social Media:
+                  </span>{" "}
+                  Social media platforms like Facebook, Twitter, and Instagram
+                  have given businesses a new way to reach their target
+                  audience.
+                </li>{" "}
+                <br />
+                <li>
+                  <span className="text-primary">
+                    The Growth of E-commerce:
+                  </span>{" "}
+                  E-commerce has made it easier for businesses to sell their
+                  products and services online, reaching a global audience.
+                </li>{" "}
+                <br />
+                <li>
+                  <span className="text-primary">
+                    The Increasing Use of Mobile Devices:
+                  </span>{" "}
+                  More and more people are using their smartphones and tablets
+                  to access the internet, which has created new opportunities
+                  for businesses to reach their target.
+                </li>
               </ul>
             </Card.Body>
           </Card>
         </Col>
 
         <Col md={6}>
-          <Card className='trad-card mark-card'>
+          <Card className="trad-card mark-card">
             <Card.Body>
-              <Card.Title><strong>Traditional Marketing</strong></Card.Title>
-              <Card.Text className='text-secondary text-overflow'>
-                Traditional marketing has also grown in recent years, albeit at a slower pace than digital marketing. This growth has been driven by a number of factors, including:
+              <Card.Title>
+                <strong>Traditional Marketing</strong>
+              </Card.Title>
+              <Card.Text className="text-secondary text-overflow">
+                Traditional marketing has also grown in recent years, albeit at
+                a slower pace than digital marketing. This growth has been
+                driven by a number of factors, including:
               </Card.Text>
               <ul>
-                <li><span className='text-primary'>The Rise of Cable and Satellite Television:</span> Cable and satellite television have given businesses a new way to reach their target audience.</li> <br />
-                <li><span className='text-primary'>The Growth of Direct Mail:</span> Direct mail has given businesses a way to target their marketing messages to specific demographics.</li> <br />
-                <li><span className='text-primary'>The Increasing use of Out-of-Home Advertising:</span> Out-of-home advertising, such as billboards and bus shelters, has given businesses a way to reach their target audience in a non-intrusive way.</li>
+                <li>
+                  <span className="text-primary">
+                    The Rise of Cable and Satellite Television:
+                  </span>{" "}
+                  Cable and satellite television have given businesses a new way
+                  to reach their target audience.
+                </li>{" "}
+                <br />
+                <li>
+                  <span className="text-primary">
+                    The Growth of Direct Mail:
+                  </span>{" "}
+                  Direct mail has given businesses a way to target their
+                  marketing messages to specific demographics.
+                </li>{" "}
+                <br />
+                <li>
+                  <span className="text-primary">
+                    The Increasing use of Out-of-Home Advertising:
+                  </span>{" "}
+                  Out-of-home advertising, such as billboards and bus shelters,
+                  has given businesses a way to reach their target audience in a
+                  non-intrusive way.
+                </li>
               </ul>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-      <h4>Comparison</h4>
-      <p>
-        Digital marketing and traditional marketing have different strengths and weaknesses. Digital marketing is more targeted and measurable, while traditional marketing can reach a larger audience. Ultimately, the best type of marketing for a business will depend on its specific goals and target audience.
+      <h4 className="text-white my-5"><strong>Conclusion: </strong></h4>
+      <p className="text-secondary">
+        Digital marketing and Traditional marketing have different strengths and
+        weaknesses. Digital marketing is more targeted and measurable, while
+        traditional marketing can reach a larger audience. Ultimately, the best
+        type of marketing for a business will depend on its specific goals and
+        target audience.
       </p>
     </Container>
   );
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,55 +5,126 @@ import img1 from "../images/Services/1.png";
 import img2 from "../images/Services/2.png";
 import img3 from "../images/Services/3.png";
 import img4 from "../images/Services/4.png";
-import content1 from "../images/Services/content1.svg";
-import gif1 from "../images/homepage-gif.gif";
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-import TestimonialContainer from "./TestimonialContainer";
+import sl1 from "../images/Services/content-marketing.gif";
+import sl2 from "../images/Services/social-media-marketing.gif";
+import sl3 from "../images/Services/search-engine-optimization.gif";
+import sl4 from "../images/Services/web-design-and-development.gif";
 import MarketingComparison from "./MarketingComparison";
+import { Card, CardActionArea, CardContent, Typography, Grid } from "@material-ui/core";
 
 const Service = () => {
   return (
-    <>
+    <div className="service">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="header-carousel p-3"
+        className="header-carousel"
       >
         <Carousel controls={false} interval={4000} pause={true}>
           <Carousel.Item>
             <img className="carousel-image" src={img1} alt="First slide" />
           </Carousel.Item>
-
           <Carousel.Item>
             <img className="carousel-image" src={img2} alt="Second slide" />
           </Carousel.Item>
-
           <Carousel.Item>
             <img className="carousel-image" src={img3} alt="Third slide" />
           </Carousel.Item>
-
           <Carousel.Item>
             <img className="carousel-image" src={img4} alt="Fourth slide" />
           </Carousel.Item>
         </Carousel>
       </motion.div>
-      <MarketingComparison/>
-      {/* <TestimonialContainer
-        name="Digital Marketing vs Traditional Marketing"
-        imgsrc={content1}
-        tagline=" “How Growth has happened in both of these marketing methods ? "
-        content={[ <h4 className="text-white"> <strong>Digital Marketing</strong> </h4>, <br/>, "Digital marketing has grown exponentially in recent years, as more and more people are",
-        "spending time online. This growth has been driven by a number of factors, including:", <br />, <br />, 
-        <span className="text-primary"> <strong> <li> The Rise of Social Media:</li></strong></span>, " Social media platforms like Facebook, Twitter, and Instagram have",
-        "given businesses a new way to reach their target audience.", <br />, <br />,
-        <span className="text-primary"> <strong> <li> The Growth of E-Commerce:</li></strong></span>, " E-commerce has made it easier for businesses to sell their products",
-        "and services online, reaching a global audience.", <br />, <br />,
-        <span className="text-primary"> <strong> <li>The Increasing use of Mobile Devices:</li></strong></span>, " More and more people are using their smartphones and",
-        "tablets to access the internet, which has created new opportunities for businesses to reach their target audience." ]}
-      />
-      <br /> <br /> */}
-    </>
+      <MarketingComparison />
+      <div className="text-center text-white my-5">
+        <h1>
+          <strong>Our Services</strong>
+        </h1>
+        <hr />
+      </div>
+      <Grid container spacing={2} justify="center">
+        <Grid item xs={12} sm={6} md={3}>
+          <Card className="service-card">
+            <CardActionArea>
+              <div className="service-icon">
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <img src={sl1} alt="Content Marketing" width="55" height="55" />
+                </motion.div>
+              </div>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Content Marketing
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  DigiVerse360 creates high-quality content that engages and informs your target audience. This content can be used to attract new customers, build relationships with existing customers, and boost your website's search engine ranking.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        {/* Repeat the same structure for the remaining service cards */}
+        <Grid item xs={12} sm={6} md={3}>
+          <Card className="service-card">
+            <CardActionArea>
+              <div className="service-icon">
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <img src={sl2} alt="Social Media Marketing" width="55" height="55" />
+                </motion.div>
+              </div>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Social Media Marketing
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  DigiVerse360 helps you manage your social media accounts, create engaging content, and measure your results. They can also help you develop social media advertising campaigns that reach your target audience and drive traffic to your website.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Card className="service-card">
+            <CardActionArea>
+              <div className="service-icon">
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <img src={sl3} alt="Search Engine Optimization" width="55" height="55" />
+                </motion.div>
+              </div>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Search Engine Optimization (SEO)
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  DigiVerse360 helps you improve your website's ranking in search engine results pages (SERPs). This can help you attract more visitors to your website, which can lead to increased sales and revenue.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Card className="service-card">
+            <CardActionArea>
+              <div className="service-icon">
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <img src={sl4} alt="Web Design and Development" width="55" height="55" />
+                </motion.div>
+              </div>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Web Design and Development
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  DigiVerse360 can help you design and develop a website that meets your business needs. They can also help you with website maintenance and updates.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+      <br />
+      <br />
+    </div>
   );
 };
 
