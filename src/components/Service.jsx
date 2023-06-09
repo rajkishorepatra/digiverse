d29788b6a5f3b7a0d9e519155d322346eed53449
aditya-2403 <47123413+aditya-2403@users.noreply.github.com>
2023-06-09 23:50:54 +0530

    ADD chnages to service page

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,8 +5,11 @@ import img1 from "../images/Services/1.png";
 import img2 from "../images/Services/2.png";
 import img3 from "../images/Services/3.png";
 import img4 from "../images/Services/4.png";
+import content1 from "../images/Services/content1.svg";
 import gif1 from "../images/homepage-gif.gif";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import TestimonialContainer from "./TestimonialContainer";
+import MarketingComparison from "./MarketingComparison";
 
 const Service = () => {
   return (
@@ -35,21 +38,21 @@ const Service = () => {
           </Carousel.Item>
         </Carousel>
       </motion.div>
-      <div className="service-content d-flex justify-content-center align-items-start flex-column p-3">
-        <h3 className="text-white">How Growth has Happened with Digital Marketing vs Traditional Marketing</h3>
-        <h4 className="text-white"><strong>Digital Marketing</strong></h4>
-        <p className="text-secondary">
-          Digital marketing has grown exponentially in recent years as more and more people are spending time online. This
-          growth has been driven by a number of factors, including:
-        </p>
-        <ul className="text-secondary">
-          <li>The rise of social media: Social media platforms like Facebook, Twitter, and Instagram have given businesses a new way to reach their target audience.</li>
-          <li>The growth of e-commerce: E-commerce has made it easier for businesses to sell their products and services online, reaching a global audience.</li>
-          <li>The increasing use of mobile devices: More and more people are using their smartphones and tablets to access the internet, which has created new opportunities for businesses to reach their target audience.</li>
-        </ul>
-        <img src={gif1} alt="digital marketing" className="content-image" />
-      </div>
-      <br /> <br />
+      <MarketingComparison/>
+      {/* <TestimonialContainer
+        name="Digital Marketing vs Traditional Marketing"
+        imgsrc={content1}
+        tagline=" “How Growth has happened in both of these marketing methods ? "
+        content={[ <h4 className="text-white"> <strong>Digital Marketing</strong> </h4>, <br/>, "Digital marketing has grown exponentially in recent years, as more and more people are",
+        "spending time online. This growth has been driven by a number of factors, including:", <br />, <br />, 
+        <span className="text-primary"> <strong> <li> The Rise of Social Media:</li></strong></span>, " Social media platforms like Facebook, Twitter, and Instagram have",
+        "given businesses a new way to reach their target audience.", <br />, <br />,
+        <span className="text-primary"> <strong> <li> The Growth of E-Commerce:</li></strong></span>, " E-commerce has made it easier for businesses to sell their products",
+        "and services online, reaching a global audience.", <br />, <br />,
+        <span className="text-primary"> <strong> <li>The Increasing use of Mobile Devices:</li></strong></span>, " More and more people are using their smartphones and",
+        "tablets to access the internet, which has created new opportunities for businesses to reach their target audience." ]}
+      />
+      <br /> <br /> */}
     </>
   );
 };
