2522be17849c336fe907b6df9e929eaca041a53d
aditya-2403 <yoyosahoo319@gmail.com>
2023-10-13 00:27:15 +0530

    ADD Demo Modal

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import MyParticles from "./components/MyParticles";
 import ScrollToTop from "./components/ScrollToTop";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 import Loader from './components/Loader';
+import demo from "./images/demo.jpeg";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga';
+import { Modal, Button } from 'react-bootstrap';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false); // Don't show the modal by default
   const location = useLocation();
 
   useEffect(() => {
@@ -21,7 +24,16 @@ const App = () => {
 
     ReactGA.initialize('UA-XXXXXXXXX-X'); // Replace 'UA-XXXXXXXXX-X' with your tracking ID
     ReactGA.pageview(window.location.pathname + window.location.search);
+
+    // Delay showing the modal by 3 seconds
+    setTimeout(() => {
+      setShowModal(true);
+    }, 3000);
   }, [location]);
+
+  const handleClose = () => {
+    setShowModal(false);
+  }
 
   return (
     <>
@@ -35,6 +47,24 @@ const App = () => {
         <Footer />
         <ScrollToTopButton />
       </div>
+
+      {/* Modal */}
+      <Modal className='demo-modal' show={showModal} onHide={handleClose} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>Demo Class</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={demo}alt="Demo Class Image" className="demo-modal-img img-fluid" />
+          <p>In the digital age, mastering the art of digital marketing is your key to success. Our course equips 
+            you with the skills to create strategies, engage your audience, and thrive in the ever-evolving digital
+             landscape. <b> Join our Demo Class Today!</b>
+</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" target='_blank' href="https://docs.google.com/forms/d/e/1FAIpQLScMyhG3b4CLyHqUTpIOytZ608ADsNppkXuNdNllt6OSfqcQGw/viewform?usp=sf_link">Register</Button>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
