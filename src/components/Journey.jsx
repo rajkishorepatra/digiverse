154d38b684e733254c608d8400666a514a232f64
rajkishorepatra <rpatrasm@gmail.com>
2024-07-25 23:08:03 +0530

    removed news article

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -6,7 +6,7 @@ import news from '../images/news.jpg';
 const Journey = () => {
   return (
     <div className="journey d-flex justify-content-center align-items-left flex-column text-white p-4">
-     <div className="d-flex justify-content-center aligm-items-center" > <img className="ceoimg" style={{width:'25%', boxShadow:'2px 2px 5px #fff',borderRadius:'20px'}} src={ceo} alt="Shubham Pani - CEO, Digiverse360" /></div> <br /> <br />
+      <div className="d-flex justify-content-center aligm-items-center" > <img className="ceoimg" style={{ width: '25%', boxShadow: '2px 2px 5px #fff', borderRadius: '20px' }} src={ceo} alt="Shubham Pani - CEO, Digiverse360" /></div> <br /> <br />
       <h1> <strong> The Journey:</strong></h1> <br />
       <p>
         In 2021, during a nationwide lockdown, I embarked on this journey
@@ -28,52 +28,50 @@ const Journey = () => {
       </p> <br />
       <h2> <strong>Expanding the Reach:</strong> </h2> <br />
       <p>Driven by my passion for digital marketing, I recognized the need to create awareness and
-            educate people about its potential. However, I encountered reluctance from some who doubted
-            the future prospects of this field in India. I took it upon myself to provide undeniable proof that
-            digital marketing not only has a bright future but also offers ample career opportunities.
-        </p> <br />
-        <h4> <strong> Digital Marketing's Future in India:</strong></h4> <br />
-        <p> <strong className="text-primary" >1. Rapid Growth:</strong> <br /> <br /> India's digital landscape is experiencing exponential growth, with millions of
-people gaining access to the internet every year. This presents an enormous potential for
-businesses to reach a vast consumer base through digital marketing strategies.</p> <br />
+        educate people about its potential. However, I encountered reluctance from some who doubted
+        the future prospects of this field in India. I took it upon myself to provide undeniable proof that
+        digital marketing not only has a bright future but also offers ample career opportunities.
+      </p> <br />
+      <h4> <strong> Digital Marketing's Future in India:</strong></h4> <br />
+      <p> <strong className="text-primary" >1. Rapid Growth:</strong> <br /> <br /> India's digital landscape is experiencing exponential growth, with millions of
+        people gaining access to the internet every year. This presents an enormous potential for
+        businesses to reach a vast consumer base through digital marketing strategies.</p> <br />
 
 
-<p> <strong className="text-primary">2. Increased Digital Adoption:</strong> <br /> <br /> The COVID-19 pandemic has accelerated the adoption of digital
-technologies in various sectors. Businesses are now prioritizing their online presence, leading to
-a surge in demand for digital marketing professionals who can help them navigate the digital
-landscape effectively.</p> <br />
+      <p> <strong className="text-primary">2. Increased Digital Adoption:</strong> <br /> <br /> The COVID-19 pandemic has accelerated the adoption of digital
+        technologies in various sectors. Businesses are now prioritizing their online presence, leading to
+        a surge in demand for digital marketing professionals who can help them navigate the digital
+        landscape effectively.</p> <br />
 
 
-<p> <strong className="text-primary">3. E-commerce Boom:</strong> <br /> <br /> India's e-commerce industry is witnessing significant growth, and digital
-marketing plays a pivotal role in driving online sales. With more businesses embracing
-e-commerce platforms, the demand for skilled digital marketers who can optimize online
-campaigns, enhance customer engagement, and drive conversions is on the rise.</p> <br />
+      <p> <strong className="text-primary">3. E-commerce Boom:</strong> <br /> <br /> India's e-commerce industry is witnessing significant growth, and digital
+        marketing plays a pivotal role in driving online sales. With more businesses embracing
+        e-commerce platforms, the demand for skilled digital marketers who can optimize online
+        campaigns, enhance customer engagement, and drive conversions is on the rise.</p> <br />
 
-<p> <strong className="text-primary">4. Rising Importance of Social Media:</strong> <br /> <br /> Social media platforms have become an integral part of
-people's lives, offering a vast opportunity for businesses to engage with their target audience.
+      <p> <strong className="text-primary">4. Rising Importance of Social Media:</strong> <br /> <br /> Social media platforms have become an integral part of
+        people's lives, offering a vast opportunity for businesses to engage with their target audience.
 
-Digital marketing strategies that leverage social media channels such as Facebook, Instagram,
-Twitter, and YouTube can help companies establish their brand presence and connect with
-potential customers.</p> <br />
+        Digital marketing strategies that leverage social media channels such as Facebook, Instagram,
+        Twitter, and YouTube can help companies establish their brand presence and connect with
+        potential customers.</p> <br />
 
-<p> <strong className="text-primary">5. Analytics and Data-Driven Insights:</strong> <br /> <br /> Digital marketing provides extensive data and analytics
-tools that enable businesses to measure the success of their campaigns, identify target
-audience behavior, and refine their marketing strategies accordingly. Skilled professionals who
-can analyze and interpret data hold a competitive edge in the industry..</p> <br />
-<h3> <strong>Conclusion:</strong> </h3>
-<p>Digital marketing in India is far from a passing trend; it is a thriving industry with immense
-potential for growth. As the CEO of Digiverse 360, I am committed to raising awareness about
-the numerous opportunities that digital marketing offers. By capitalizing on the country's digital
-transformation and fostering innovation, we can empower individuals to build rewarding careers
-and help businesses thrive in the digital age. Together, let's embrace the power of digital
-marketing and shape a brighter future for India.</p> <br /> <br />
+      <p> <strong className="text-primary">5. Analytics and Data-Driven Insights:</strong> <br /> <br /> Digital marketing provides extensive data and analytics
+        tools that enable businesses to measure the success of their campaigns, identify target
+        audience behavior, and refine their marketing strategies accordingly. Skilled professionals who
+        can analyze and interpret data hold a competitive edge in the industry..</p> <br />
+      <h3> <strong>Conclusion:</strong> </h3>
+      <p>Digital marketing in India is far from a passing trend; it is a thriving industry with immense
+        potential for growth. As the CEO of Digiverse 360, I am committed to raising awareness about
+        the numerous opportunities that digital marketing offers. By capitalizing on the country's digital
+        transformation and fostering innovation, we can empower individuals to build rewarding careers
+        and help businesses thrive in the digital age. Together, let's embrace the power of digital
+        marketing and shape a brighter future for India.</p> <br /> <br />
 
-<caption className="text-white">~ Shubham Pani - CEO, Digiverse360</caption>
-<a style={{textDecoration:'none'}} href="https://www.pratidinrajdhani.in/single/belief-in-myself,-inspiration-from-parents-has-brought-me-to-this-stage-shubham" target="_blank" rel="noreferrer" >🔗 News Article Feature ➤</a> <br />
- 
-<a href="https://www.pratidinrajdhani.in/single/belief-in-myself,-inspiration-from-parents-has-brought-me-to-this-stage-shubham"><img  className="news" style={{width:'30%'}} src={news} alt="News Article Feature" /> </a>  <br />
-<NavLink className='btn btn-outline-primary' to='/about'>Go Back</NavLink> <br />
- <br /> <br /> <br /> <br /> <br />
+      <caption className="text-white">~ Shubham Pani - CEO, Digiverse360</caption>
+
+      <NavLink className='btn btn-outline-primary' to='/about'>Go Back</NavLink> <br />
+      <br /> <br /> <br /> <br /> <br />
 
     </div>
   );
